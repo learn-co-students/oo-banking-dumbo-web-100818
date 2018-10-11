7a0efe37702464c7e4be2558ea0dbb4067bf723a
Gianna de la cruz <giannadelacruz@Giannas-MacBook-Pro.local>
2018-10-11 17:13:44 -0400

    completed lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
 
+attr_accessor :balance, :status, :display_balance
+attr_reader :name
+
+  def initialize (name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit)
+    @balance += deposit
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+
+  def valid?
+    @status == "open" && @balance > 0
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
+
+
 end
