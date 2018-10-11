d8bbab84e412d2d4b57d12c09454be61ebf9a0ef
github username <chao.alexander.123@gmail.com>
2018-10-11 16:35:24 -0400

    create bank_account and transfer classes with the required methods

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
+require 'pry'
+require_relative './bank_account'
+
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && @status == "pending" && @sender.balance > @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
+
 end
+#
+# avi = BankAccount.new("Avi")
+# amanda = BankAccount.new("Amanda")
+# transfer = Transfer.new(avi, amanda, 50)
+# transfer.execute_transaction
